0c819dfcb674676b70abe8c7cc598ff4b6418bc4
mrzjd <390303304@qq.com>
2025-08-22 11:22:16 +0800

    fix: suspense loading

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -4,7 +4,7 @@ import WorkBox from "./components/workbox";
 
 export default function EditPage() {
   return (
-    <Suspense fallback={'Some Error Ocurred.'}>
+    <Suspense fallback={'Loading...'}>
       <div className="font-sans bg-gray-100 w-[100vw] h-[100vh] overflow-hidden grid">
         <div className="bg-white rounded-md m-1 overflow-hidden relative">
           <Header />
