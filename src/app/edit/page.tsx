10194a7b520dc0499776d5eda2d6c806945c4d29
mrzjd <390303304@qq.com>
2025-08-22 11:08:46 +0800

    fix: build error

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,13 +1,16 @@
+import { Suspense } from "react";
 import Header from "./components/header";
 import WorkBox from "./components/workbox";
 
 export default function EditPage() {
   return (
-    <div className="font-sans bg-gray-100 w-[100vw] h-[100vh] overflow-hidden grid">
-      <div className="bg-white rounded-md m-1 overflow-hidden relative">
-        <Header />
-        <WorkBox />
+    <Suspense fallback={'Some Error Ocurred.'}>
+      <div className="font-sans bg-gray-100 w-[100vw] h-[100vh] overflow-hidden grid">
+        <div className="bg-white rounded-md m-1 overflow-hidden relative">
+          <Header />
+          <WorkBox />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
